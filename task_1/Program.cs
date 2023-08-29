ee2a052de6bb63697255189949f6b3f16ae74bc9
evgen <makeevgen1975@gmail.com>
2023-08-29 14:54:08 +0300

    add task1

---
+++ task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Write("Введите число: ");
+string input1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input1);
+Console.Write("Введите ещё одно число: ");
+string input2 = Console.ReadLine();
+int number2 = Convert.ToInt32(input2);
+if (number1 > number2)
+{
+    Console.WriteLine("max = " + number1);
+}
+else if (number1 == number2)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    Console.WriteLine("max = " + number2);
+}
+
