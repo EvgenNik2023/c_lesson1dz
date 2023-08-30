22a37daa7e007cdb81aa732e6b93c3de3b1971a5
evgen <makeevgen1975@gmail.com>
2023-08-30 15:37:29 +0300

    add task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+string digit = Console.ReadLine();
+int N = Convert.ToInt32(digit);
+
+int count = 2;
+
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + ", ");
+    }
+    count++;
+}
