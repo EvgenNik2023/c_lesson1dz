51cec4aef4d547bb7453dca75f526dadde61131e
evgen <makeevgen1975@gmail.com>
2023-08-30 15:16:39 +0300

    add new files

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+string one = Console.ReadLine();
+int first = Convert.ToInt32(one);
+
+Console.Write("Введите ещё одно число: ");
+string two = Console.ReadLine();
+int second = Convert.ToInt32(two);
+
+Console.Write("Введите ещё одно число: ");
+string three = Console.ReadLine();
+int third = Convert.ToInt32(three);
+
+int max = first;
+
+if (max < second)
+{
+    max = second;
+}
+
+if (max < third)
+{
+    max = third;
+}
+Console.WriteLine(max);
---
+++ task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+string one = Console.ReadLine();
+int first = Convert.ToInt32(one);
+
+if (first % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
